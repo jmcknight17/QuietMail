1560235d7ba92947bb1c60e8f90cebccfcebb2b0
jaymcknight <jmcknight17@qub.ac.uk>
2025-08-20 21:16:03 +0100

    UnversionedFiles

---
+++ QuietMail.Api/QuietMail.Api/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My Email API", Version = "v1" });
+});
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My Email API V1");
+        c.RoutePrefix = string.Empty;
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors(builder =>
+    builder.WithOrigins("https://localhost:3000", "http://localhost:3000")
+           .AllowAnyHeader()
+           .AllowAnyMethod()
+           .AllowCredentials());
+
+app.MapControllers();
+
+app.UseCors();
+
+app.Run();
