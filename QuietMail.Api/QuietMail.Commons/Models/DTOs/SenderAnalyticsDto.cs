2a454b34179d897b339e70122c95a61a3fed7d27
jaymcknight <jmcknight17@qub.ac.uk>
2025-11-30 21:05:22 +0000

    New Common folder, forgot to check in the old csproj

---
+++ QuietMail.Api/QuietMail.Commons/Models/DTOs/SenderAnalyticsDto.cs
@@ -0,0 +1,20 @@
+﻿namespace QuietMail.Api.Models;
+
+public class SenderAnalyticsDto
+{
+    public string Domain { get; set; }
+    public List<IndividualSenderDto> IndividualSenders { get; set; } 
+    public int EmailCount { get; set; }
+    public int OpenedCount { get; set; }
+    public double OpenedPercent { get; set; }
+}
+
+public class IndividualSenderDto
+{
+    public string Email { get; set; }
+    public int EmailCount { get; set; }
+    public int OpenedCount { get; set; }
+    public double OpenedPercent { get; set; }
+    public bool IsMailList { get; set; }
+}
+
